d05a133216cfebd7ced9bc53bcb1a3d66dcec11e
Jon Keam <jpkeam@gmail.com>
2025-02-05 01:21:39 -0500

    remove platform

--- wasi/build.sh
+++ wasi/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 make build
-buildah build --annotation "module.wasm.image/variant=compat" --platform wasi/wasm -t quay.io/jkeam/wasi
+buildah build --annotation "module.wasm.image/variant=compat" -t quay.io/jkeam/wasi
